454b04b288763ec8a8820b985e20aa9546f292f5
Haribhuva31 <dev+234@compubrain.in>
2026-08-21 18:56:05 +0530

    start next.js learning

---
+++ frontend_next/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+import Image from "./assets/image.webp"
+
+export default function Homepage() {
+    return(
+        <div className="color item-center justify-center bg-orange-400 font-family-rubik font-sans dark:bg-black">
+            <img src="./assets/image.webp" alt="" />
+            <p className=""><span className="color #fc1515">Hello</span> world</p>
+        </div>
+    )
+}
